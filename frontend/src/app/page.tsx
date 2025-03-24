5a785a17b4186ad20a39ddfd95a4f0d1e146dbc3
leotonezi <leoademir0@gmail.com>
2025-03-23 23:17:49 -0300

    chore: add frontend structure (nextjs)

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center text-3xl">
+      Hello, Sonic Library! 🚀
+    </main>
+  );
+}
